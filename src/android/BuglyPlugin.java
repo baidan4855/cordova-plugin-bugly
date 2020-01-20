2f9abe227d4a51524903a7dae3ec8adc21113407
Thinking Song <123wuming_123@163.com>
2020-01-20 21:08:59 +0800

    update bugly plugin

--- src/android/BuglyPlugin.java
+++ src/android/BuglyPlugin.java
@@ -9,13 +9,13 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
 
-import android.app.Application;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebView;
+import android.app.Application;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,18 +26,21 @@ import com.tencent.bugly.Bugly;
 import java.util.List;
 
 public class BuglyPlugin extends CordovaPlugin {
+
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         
-        Log.info("Bugly running.......");
+        Log.i("bugly", "Bugly running.......");
     
         try {
-            ApplicationInfo applicationInfo = cordova.getActivity().getApplicationInfo(this.getPackageName(), PackageManager.GET_META_DATA);
+            Application application = cordova.getActivity().getApplication();
+            
+            ApplicationInfo applicationInfo = application.getPackageManager().getApplicationInfo(application.getPackageName(), PackageManager.GET_META_DATA);
             Bundle bundle = applicationInfo.metaData;
             String buglyAppId = bundle.getString("BUGLY_APPID");
             Boolean bugylyDebug = bundle.getBoolean("BUGLY_ENABLE_DEBUG");
-            Bugly.init(getApplicationContext(), buglyAppId, bugylyDebug);
+            Bugly.init(application.getApplicationContext(), buglyAppId, bugylyDebug);
         } catch (PackageManager.NameNotFoundException e) {
             Log.e("bugly", "Failed to load meta-data, NameNotFound: " + e.getMessage());
         }
